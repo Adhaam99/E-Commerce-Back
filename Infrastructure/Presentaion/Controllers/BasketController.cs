065d80bb9fa24a4c1651a72088660416d4b9d22a
adham yehia <adhamyehia194@gmail.com>
2025-04-27 18:03:36 +0300

    Part 13 Basket Module - Controller

---
+++ Infrastructure/Presentaion/Controllers/BasketController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared.DataTransferObjects.BasketModuleDtos;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BasketController(IServiceManager _serviceManager) : ControllerBase
+    {
+
+        // Get Basket
+
+        [HttpGet] // GET: api/basket
+        public async Task<ActionResult<BasketDto>> GetBasket(string Key)
+        {
+            var Basket = await _serviceManager.BasketService.GetBasketAsync(Key);
+            return Ok(Basket);
+        }
+
+        // Create OR Update a new basket
+
+        [HttpPost] // POST: api/basket
+        public async Task<ActionResult<BasketDto>> CreateOrUpdateBasket(BasketDto basket)
+        {
+            var Basket = await _serviceManager.BasketService.CreatedOrUpdateBasketAsync(basket);
+            return Ok(Basket);
+        }
+
+        // Delete a basket
+
+        [HttpDelete("{Key}")] // DELETE: api/basket
+        public async Task<ActionResult<bool>> DeleteBasket(string Key)
+        {
+            var Result = await _serviceManager.BasketService.DeleteBasketAsync(Key);
+            return Ok(Result);
+        }
+    }
+}
