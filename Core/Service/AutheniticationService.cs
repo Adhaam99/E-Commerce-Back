cab313562e10193b2ba08e2a5b5c52250c07ca57
adham yehia <adhamyehia194@gmail.com>
2025-05-01 01:46:48 +0300

    Part 05 Authentication Service [Login & Register]

---
+++ Core/Service/AutheniticationService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainLayer.Exceptions;
+using DomainLayer.Models.IdentityModule;
+using Microsoft.AspNetCore.Identity;
+using ServiceAbstraction;
+using Shared.DataTransferObjects.IdentityDtos;
+
+namespace Service
+{
+    public class AutheniticationService(UserManager<ApplicationUser> _userManager) : IAuthenticationService
+    {
+        public async Task<UserDto> Login(LoginDto loginDto)
+        {
+            // Check If Email Is Exsist
+            var User = await _userManager.FindByEmailAsync(loginDto.Email) ?? throw new UserNotFoundException(loginDto.Email);
+
+            // Check If Password Is Correct
+            var isPasswordValid = await _userManager.CheckPasswordAsync(User, loginDto.Password);
+            if (isPasswordValid)
+            {
+                return new UserDto()
+                {
+                    DisplayName = User.DisplayName,
+                    Email = User.Email ?? "",
+                    Token = CreateTokenAsync(User) // Generate Token Here
+                };
+            }
+            else
+            {
+                throw new UnAuthorizedException();
+            }
+        }
+
+
+        public async Task<UserDto> Register(RegisterDto registerDto)
+        {
+            // Mapping RegisterDto To ApplicationUser
+            var User = new ApplicationUser()
+            {
+                Email = registerDto.Email,
+                UserName = registerDto.UserName,
+                PhoneNumber = registerDto.PhoneNumber,
+                DisplayName = registerDto.DisplayName
+            };
+
+            // Create User [Application User]
+            var result = await _userManager.CreateAsync(User, registerDto.Password);
+            if (result.Succeeded)
+            {
+                // Generate Token Here
+                return new UserDto()
+                {
+                    DisplayName = User.DisplayName,
+                    Email = User.Email ?? "",
+                    Token = CreateTokenAsync(User)
+                };
+            }
+            else
+            {
+                // Get Errors From Result
+                var errors = result.Errors.Select(e => e.Description).ToList();
+                throw new BadRequestException(errors);
+            }
+        }
+        private string CreateTokenAsync(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Core/Service/ServiceManager.cs
+++ Core/Service/ServiceManager.cs
@@ -5,16 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using DomainLayer.Contracts;
+using DomainLayer.Models.IdentityModule;
+using Microsoft.AspNetCore.Identity;
 using ServiceAbstraction;
 
 namespace Service
 {
-    public class ServiceManager(IUnitOfWork _unitOfWork, IMapper _mapper, IBasketRepository _basketRepository) : IServiceManager
+    public class ServiceManager(IUnitOfWork _unitOfWork, IMapper _mapper, IBasketRepository _basketRepository, UserManager<ApplicationUser> _userManager) : IServiceManager
     {
         private readonly Lazy<IProductService> _LazyProductService = new Lazy<IProductService>(() => new ProductService(_unitOfWork, _mapper));
         public IProductService ProductService => _LazyProductService.Value;
 
         private readonly Lazy<IBasketService> _LazyBasketService = new Lazy<IBasketService>(() => new BasketService(_basketRepository, _mapper));
         public IBasketService BasketService => _LazyBasketService.Value;
+
+        private readonly Lazy<IAuthenticationService> _LazyAuthenticationService = new Lazy<IAuthenticationService>(() => new AutheniticationService(_userManager));
+        public IAuthenticationService AuthenticationService => _LazyAuthenticationService.Value;
     }
 }
