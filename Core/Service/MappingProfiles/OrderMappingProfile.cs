eaa53a597f94998c6503dfe44c3beaa88de1a6d6
adham yehia <adhamyehia194@gmail.com>
2025-05-17 18:51:21 +0300

    Part 04 Order Service [ Create Order ]

---
+++ Core/Service/MappingProfiles/OrderMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DomainLayer.Models.IdentityModule;
+using DomainLayer.Models.OrderModule;
+using Shared.DataTransferObjects.IdentityDtos;
+using Shared.OrderDtos;
+
+namespace Service.MappingProfiles
+{
+    public class OrderMappingProfile : Profile
+    {
+        public OrderMappingProfile() 
+        {
+            CreateMap<AddressDto, OrderAddress>().ReverseMap();
+            CreateMap<OrderToReturnDto, Order>().ReverseMap();
+        }
+    }
+}
---
+++ Core/Service/OrderService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DomainLayer.Contracts;
+using DomainLayer.Exceptions;
+using DomainLayer.Models.OrderModule;
+using DomainLayer.Models.ProductModule;
+using ServiceAbstraction;
+using Shared.OrderDtos;
+
+namespace Service
+{
+    public class OrderService(IMapper _mapper, IBasketRepository _basketRepository, IUnitOfWork _unitOfWork) : IOrderService
+    {
+        public async Task<OrderToReturnDto> CreateOrderAsync(OrderDto orderDto, string email)
+        {
+            // Map Address To Order Address
+            var orderAddress = _mapper.Map<OrderAddress>(orderDto.Address);
+
+            // Get Basket
+            var basket = await _basketRepository.GetCustomerBasketAsync(orderDto.BasketId) 
+                ?? throw new BasketNotFoundException(orderDto.BasketId);
+
+            // Create OrderItem List 
+            List<OrderItem> orderItems = [];
+            var ProductRepo = _unitOfWork.GetRepository<Product, int>();
+
+            foreach (var item in orderItems)
+            {
+                var Product = await ProductRepo.GetByIdAsync(item.Id)
+                    ?? throw new ProductNotFoundException(item.Id);
+                orderItems.Add(CreateOrderItem(item, Product));
+
+            };
+
+            // Get Delivry Method
+            var deliveryMethod = await _unitOfWork.GetRepository<DeliveryMethod, int>()
+                .GetByIdAsync(orderDto.DeliveryMethodId)
+                ?? throw new DeliveryMethodNotFoundException(orderDto.DeliveryMethodId);
+
+            // Calculate Sub Total
+            var subTotal = orderItems.Sum(item => item.Price * item.Quantity);
+
+            var Order = new Order(email, orderAddress, deliveryMethod, orderItems, subTotal);
+
+            await _unitOfWork.GetRepository<Order, Guid>().AddAsync(Order);
+
+            await _unitOfWork.SaveChangesAsync();
+
+            return _mapper.Map<OrderToReturnDto>(Order);
+        }
+
+        private static OrderItem CreateOrderItem(OrderItem item, Product Product)
+        {
+            var orderItem = new OrderItem()
+            {
+                Product = new ProductItemOrdered() { ProductId = Product.Id, ProductName = Product.Name, PictureUrl = Product.PictureUrl }
+                Price = Product.Price,
+                Quantity = item.Quantity
+            };
+
+            return orderItem;
+        }
+    }
+}
